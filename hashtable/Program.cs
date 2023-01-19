b3566fff90b45662441e8f02a8a842a8b281c133
anushkachouhan029 <anushkachouhan029>
2023-01-19 23:15:54 +0530

    added hashtable

---
+++ hashtable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace HashTable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the HashTable Program \n================================");
+            LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
+            string sentence = " to be or not to be";
+            string[] words = sentence.ToLower().Split(" ");
+            foreach (string word in words)
+            {
+                int value = linkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else
+                {
+                    value = +1;
+                }
+                linkedHashMap.Add(word, value);
+            }
+            int frequency = linkedHashMap.Get("not");
+            Console.WriteLine(frequency);
+        }
+    }
+}
